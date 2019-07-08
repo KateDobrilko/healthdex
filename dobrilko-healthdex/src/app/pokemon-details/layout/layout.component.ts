76df38677b0ef4cf566cca5b30809826ffe6b4b5
KateDobrilko <katedob1595@gmail.com>
2019-07-08 23:46:44 +0300

    removing unneeded styles

--- dobrilko-healthdex/src/app/pokemon-details/layout/layout.component.ts
+++ dobrilko-healthdex/src/app/pokemon-details/layout/layout.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pokemon-details-layout',
-  templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  templateUrl: './layout.component.html'
 })
 export class PokemonDetailsLayoutComponent implements OnInit {
 
